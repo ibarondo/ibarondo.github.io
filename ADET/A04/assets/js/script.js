c840fe6fd08b358dee8883e1186121e91f876835
ibarondo <your.email@example.com>
2025-05-03 19:33:32 +0800

    added A04

--- ADET/A04/assets/js/script.js
+++ ADET/A04/assets/js/script.js
@@ -174,7 +174,7 @@ var products = [
         ]
     },
     {
-        "category": "Bottom",
+        "category": "Bottoms",
         "categoryImg": "bottom.png",
         "contents": [
             {
@@ -226,12 +226,122 @@ var products = [
                 "image": "bottom8.png"
             }
         ]
+    },
+    {
+        "category": "Shoes",
+        "categoryImg": "shoes.png",
+        "contents": [
+            {
+                "name": "Two Tone Point Toe Flats",
+                "code": "SH001",
+                "price": "799",
+                "image": "shoes1.png"
+            },
+            {
+                "name": "Wine Red Pointed Toe",
+                "code": "SH002",
+                "price": "689",
+                "image": "shoes2.png"
+            },
+            {
+                "name": "Solid Color Ankle Boots",
+                "code": "SH003",
+                "price": "899",
+                "image": "shoes3.png"
+            },
+            {
+                "name": "White Sneakers For Women",
+                "code": "SH004",
+                "price": "749",
+                "image": "shoes4.png"
+            },
+            {
+                "name": "Black High Heels",
+                "code": "SH005",
+                "price": "849",
+                "image": "shoes5.png"
+            },
+            {
+                "name": "Harness Cutout Open Toe",
+                "code": "SH006",
+                "price": "719",
+                "image": "shoes6.png"
+            },
+            {
+                "name": "Sports White Shoes",
+                "code": "SH007",
+                "price": "679",
+                "image": "shoes7.png"
+            },
+            {
+                "name": "Elegant Ankle Strap Heels",
+                "code": "SH008",
+                "price": "869",
+                "image": "shoes8.png"
+            }
+        ]
+    },
+    {
+        "category": "Accessories",
+        "categoryImg": "aj.png",
+        "contents": [
+            {
+                "name": "Flower Earrings",
+                "code": "AC001",
+                "price": "159",
+                "image": "aj1.png"
+            },
+            {
+                "name": "Beaded Bracelets",
+                "code": "AC002",
+                "price": "129",
+                "image": "aj2.png"
+            },
+            {
+                "name": "Embroidered Baseball Cap",
+                "code": "AC003",
+                "price": "199",
+                "image": "aj3.png"
+            },
+            {
+                "name": "Shaped Plastic Hair Clips",
+                "code": "AC004",
+                "price": "89",
+                "image": "aj4.png"
+            },
+            {
+                "name": "Cherry Charm Necklace",
+                "code": "AC005",
+                "price": "179",
+                "image": "aj5.png"
+            },
+            {
+                "name": "Fashionable Ring Set",
+                "code": "AC006",
+                "price": "149",
+                "image": "aj6.png"
+            },
+            {
+                "name": "Retro Leather Headband",
+                "code": "AC007",
+                "price": "119",
+                "image": "aj7.png"
+            },
+            {
+                "name": "Gold Strap Quartz Watch",
+                "code": "AC008",
+                "price": "399",
+                "image": "aj8.png"
+            }
+        ]
     }
+
 ]
 
 let total = 0;
 const taxRate = 0.01;
 
+// loading categories
 function loadCategories() {
     var categoriesContainer = document.getElementById("categories");
 
@@ -239,13 +349,14 @@ function loadCategories() {
         categoriesContainer.innerHTML +=
             `<div onclick="loadProducts('` + index + `')" class="card-icon">
                 <div class="icon">
-                    <img src="assets/images/` + product.categoryImg + `">
+                    <img src="assets/images/products/` + product.categoryImg + `">
                 </div>
                 <span>` + product.category + `</span>
             </div>`;
     });
 }
 
+// loading products
 var categoryIndex = 0;
 function loadProducts(categoryIndex) {
     var productContainer = document.getElementById("productContainer");
@@ -253,7 +364,7 @@ function loadProducts(categoryIndex) {
 
     products[categoryIndex].contents.forEach(content => {
         productContainer.innerHTML += `<div onclick="addToOrder('` + content.code + `', '` + content.price + `')" class="card-item">
-        <img src="assets/images/`+ content.image + `" alt="` + content.name + `" class="img-fluid">
+        <img src="assets/images/products/`+ content.image + `" alt="` + content.name + `" class="img-fluid">
         <div class="card-label">
             <span class="name">`+ content.name + `</span>
             <span class="price">₱`+ content.price + `</span>
@@ -262,21 +373,23 @@ function loadProducts(categoryIndex) {
     });
 };
 
+// add to order 
 function addToOrder(code, price) {
     var receiptContainer = document.getElementById("receipt");
-    
+
     total = parseFloat(total) + parseFloat(price);
 
     receiptContainer.innerHTML += `<div class="card-order">
-                                    <span>`+code+`</span>
-                                    <span>₱`+price+`</span>
+                                    <span>`+ code + `</span>
+                                    <span>₱`+ price + `</span>
                                 </div>`;
 
     updatePaymentSummary();
 };
 
+// update summay or total payment
 function updatePaymentSummary() {
-   
+
     const subtotal = total;
     const vat = subtotal * taxRate;
     const totalPayment = subtotal + vat;
@@ -285,6 +398,32 @@ function updatePaymentSummary() {
     document.getElementById("VAT").innerText = "₱" + vat.toFixed(2);
     document.getElementById("totalPayment").innerText = "₱" + totalPayment.toFixed(2);
 }
+
+// changing logo depends on system theme
+var body = document.getElementById("body");
+var dark = document.getElementById("dark");
+var light = document.getElementById("light");
+
+function setThemeAndLogo(isDark) {
+    const theme = isDark ? "dark" : "light";
+    body.setAttribute("data-bs-theme", theme);
+
+    if (isDark) {
+        dark.style.display = "none";
+        light.style.display = "block";
+    } else {
+        dark.style.display = "block";
+        light.style.display = "none";
+        
+    }
+}
+
+setThemeAndLogo(window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    setThemeAndLogo(event.matches);
+});
+
 
 loadCategories();
 
