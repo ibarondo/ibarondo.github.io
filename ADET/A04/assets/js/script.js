90cfad3d5f0fe68818f3e71e1209b089fed3eb63
Jannah Ibarondo <jannah.ibarondo13@gmail.com>
2025-05-17 02:54:14 +0800

    A06 Activity

--- ADET/A04/assets/js/script.js
+++ ADET/A04/assets/js/script.js
@@ -1,342 +1,32 @@
-var products = [
-    {
-        "category": "Dresses",
-        "categoryImg": "dress.png",
-        "contents": [
-            {
-                "name": "Floral Print Dress",
-                "code": "DR001",
-                "price": "999",
-                "image": "dress1.png"
-            },
-            {
-                "name": "Off Shoulder Pleated Dress",
-                "code": "DR002",
-                "price": "950",
-                "image": "dress2.png"
-            },
-            {
-                "name": "Crisscross Tie Backless Dress",
-                "code": "DR003",
-                "price": "925",
-                "image": "dress3.png"
-            },
-            {
-                "name": "Mesh Strap Romantic Dress",
-                "code": "DR004",
-                "price": "899",
-                "image": "dress4.png"
-            },
-            {
-                "name": "Simple Strapless Dress",
-                "code": "DR005",
-                "price": "875",
-                "image": "dress5.png"
-            },
-            {
-                "name": "Black Maxi Dress",
-                "code": "DR006",
-                "price": "850",
-                "image": "dress6.png"
-            },
-            {
-                "name": "Brown Halter Neck Dress",
-                "code": "DR007",
-                "price": "825",
-                "image": "dress7.png"
-            },
-            {
-                "name": "Green Velvet Dress",
-                "code": "DR008",
-                "price": "799",
-                "image": "dress8.png"
-            },
-            {
-                "name": "High Slit Tie Dye Pink Dress",
-                "code": "DR009",
-                "price": "699",
-                "image": "dress9.png"
-            }
-        ]
-    },
-    {
-        "category": "Tops",
-        "categoryImg": "top.png",
-        "contents": [
-            {
-                "name": "Split Hem Tube Green Top",
-                "code": "TP001",
-                "price": "399",
-                "image": "top1.png"
-            },
-            {
-                "name": "Casual Sleeve Cropped Top",
-                "code": "TP002",
-                "price": "375",
-                "image": "top2.png"
-            },
-            {
-                "name": "Puff Sleeve",
-                "code": "TP003",
-                "price": "350",
-                "image": "top3.png"
-            },
-            {
-                "name": "Off-Shoulder Top",
-                "code": "TP004",
-                "price": "325",
-                "image": "top4.png"
-            },
-            {
-                "name": "Backless Halter Top",
-                "code": "TP005",
-                "price": "310",
-                "image": "top5.png"
-            },
-            {
-                "name": "Backless Bandana Top",
-                "code": "TP006",
-                "price": "299",
-                "image": "top6.png"
-            },
-            {
-                "name": "Knotted Front 2-In-1 T-Shirt",
-                "code": "TP007",
-                "price": "285",
-                "image": "top7.png"
-            },
-            {
-                "name": "Doll Bowknot Decor Puff Sleeve",
-                "code": "TP008",
-                "price": "270",
-                "image": "top8.png"
-            },
-            {
-                "name": "Y2K Leopard Halter Neck Top",
-                "code": "TP009",
-                "price": "250",
-                "image": "top9.png"
-            }
-        ]
-    },
-    {
-        "category": "Bags",
-        "categoryImg": "bag.png",
-        "contents": [
-            {
-                "name": "Minimalist Leather Shoulder Bag",
-                "code": "BG001",
-                "price": "399",
-                "image": "bag1.png"
-            },
-            {
-                "name": "Chain Strap Crossbody Bag",
-                "code": "BG002",
-                "price": "375",
-                "image": "bag2.png"
-            },
-            {
-                "name": "Trendy Crescent Hobo Bag",
-                "code": "BG003",
-                "price": "350",
-                "image": "bag3.png"
-            },
-            {
-                "name": "Mini Flap Top Handle Bag",
-                "code": "BG004",
-                "price": "325",
-                "image": "bag4.png"
-            },
-            {
-                "name": "Croc Embossed Bucket Bag",
-                "code": "BG005",
-                "price": "310",
-                "image": "bag5.png"
-            },
-            {
-                "name": "Soft Ruched Shoulder Purse",
-                "code": "BG006",
-                "price": "299",
-                "image": "bag6.png"
-            },
-            {
-                "name": "Shoulder Tote Bag",
-                "code": "BG007",
-                "price": "199",
-                "image": "bag7.png"
-            },
-            {
-                "name": "Minimalist Crocodile Embossed Bag",
-                "code": "BG008",
-                "price": "259",
-                "image": "bag8.png"
-            }
-        ]
-    },
-    {
-        "category": "Bottoms",
-        "categoryImg": "bottom.png",
-        "contents": [
-            {
-                "name": "High Waist Pants",
-                "code": "BT001",
-                "price": "459",
-                "image": "bottom1.png"
-            },
-            {
-                "name": "Summer White Hem Skirt",
-                "code": "BT002",
-                "price": "399",
-                "image": "bottom2.png"
-            },
-            {
-                "name": "Floral Print A-Line Skirt",
-                "code": "BT003",
-                "price": "365",
-                "image": "bottom3.png"
-            },
-            {
-                "name": "Double Pocket Straight Pants",
-                "code": "BT004",
-                "price": "389",
-                "image": "bottom4.png"
-            },
-            {
-                "name": "Retro High Waist Mini Skirt",
-                "code": "BT005",
-                "price": "349",
-                "image": "bottom5.png"
-            },
-            {
-                "name": "High Waist Flared Pants",
-                "code": "BT006",
-                "price": "379",
-                "image": "bottom6.png"
-            },
-            {
-                "name": " Wide Leg Straight Pants",
-                "code": "BT007",
-                "price": "279",
-                "image": "bottom7.png"
-            },
-            {
-                "name": "Tweed High Waist Mini Skirt",
-                "code": "BT008",
-                "price": "359",
-                "image": "bottom8.png"
-            }
-        ]
-    },
-    {
-        "category": "Shoes",
-        "categoryImg": "shoes.png",
-        "contents": [
-            {
-                "name": "Two Tone Point Toe Flats",
-                "code": "SH001",
-                "price": "799",
-                "image": "shoes1.png"
-            },
-            {
-                "name": "Wine Red Pointed Toe",
-                "code": "SH002",
-                "price": "689",
-                "image": "shoes2.png"
-            },
-            {
-                "name": "Solid Color Ankle Boots",
-                "code": "SH003",
-                "price": "899",
-                "image": "shoes3.png"
-            },
-            {
-                "name": "White Sneakers For Women",
-                "code": "SH004",
-                "price": "749",
-                "image": "shoes4.png"
-            },
-            {
-                "name": "Black High Heels",
-                "code": "SH005",
-                "price": "849",
-                "image": "shoes5.png"
-            },
-            {
-                "name": "Harness Cutout Open Toe",
-                "code": "SH006",
-                "price": "719",
-                "image": "shoes6.png"
-            },
-            {
-                "name": "Sports White Shoes",
-                "code": "SH007",
-                "price": "679",
-                "image": "shoes7.png"
-            },
-            {
-                "name": "Elegant Ankle Strap Heels",
-                "code": "SH008",
-                "price": "869",
-                "image": "shoes8.png"
-            }
-        ]
-    },
-    {
-        "category": "Accessories",
-        "categoryImg": "aj.png",
-        "contents": [
-            {
-                "name": "Flower Earrings",
-                "code": "AC001",
-                "price": "159",
-                "image": "aj1.png"
-            },
-            {
-                "name": "Beaded Bracelets",
-                "code": "AC002",
-                "price": "129",
-                "image": "aj2.png"
-            },
-            {
-                "name": "Gold Strap Quartz Watch",
-                "code": "AC003",
-                "price": "199",
-                "image": "aj3.png"
-            },
-            {
-                "name": "Embroidered Baseball Cap ",
-                "code": "AC004",
-                "price": "89",
-                "image": "aj4.png"
-            },
-            {
-                "name": "Shaped Plastic Hair Clips ",
-                "code": "AC005",
-                "price": "119",
-                "image": "aj5.png"
-            },
-            {
-                "name": "Cherry Charm Necklace ",
-                "code": "AC006",
-                "price": "129",
-                "image": "aj6.png"
-            },
-            {
-                "name": "Fashionable Ring Set",
-                "code": "AC007",
-                "price": "99",
-                "image": "aj7.png"
-            },
-            {
-                "name": "Retro Leather Headband",
-                "code": "AC008",
-                "price": "79",
-                "image": "aj8.png"
-            }
-        ]
-    }
+var products = [];
+var categories = [];
 
-]
+const getAllCategories = async () => {
+    fetch('http://localhost/ibarondo.github.io/ADET/A06/categories.php')
+    .then(response => response.json())
+    .then(data => {
+        categories = data;
+        loadCategories();
+    });
+}
+
+const getAllProducts = async (categoryID)=> {
+    fetch('http://localhost/ibarondo.github.io/ADET/A06/products.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ categoryID })
+    })
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        loadProducts();
+    });
+}
+
+getAllCategories();
+
 
 let total = 0;
 const taxRate = 0.01;
@@ -345,29 +35,28 @@ const taxRate = 0.01;
 function loadCategories() {
     var categoriesContainer = document.getElementById("categories");
 
-    products.forEach((product, index) => {
+    categories.forEach((category) => {
         categoriesContainer.innerHTML +=
-            `<div onclick="loadProducts('` + index + `')" class="card-icon">
+            `<div onclick="getAllProducts('` + category.categoryID + `')" class="card-icon">
                 <div class="icon">
-                    <img src="assets/images/products/` + product.categoryImg + `">
+                    <img src="assets/images/products/` + category.categoryImage + `">
                 </div>
-                <span>` + product.category + `</span>
+                <span>` + category.categoryName + `</span>
             </div>`;
     });
 }
 
 // loading products
-var categoryIndex = 0;
-function loadProducts(categoryIndex) {
+function loadProducts(categoryID) {
     var productContainer = document.getElementById("productContainer");
     productContainer.innerHTML = "";
 
-    products[categoryIndex].contents.forEach(content => {
-        productContainer.innerHTML += `<div onclick="addToOrder('` + content.code + `', '` + content.price + `')" class="card-item">
-        <img src="assets/images/products/`+ content.image + `" alt="` + content.name + `" class="img-fluid">
+    products.forEach(product => {
+        productContainer.innerHTML += `<div onclick="addToOrder('` + product.code + `', '` + product.price + `')" class="card-item">
+        <img src="assets/images/products/`+ product.image + `" alt="` + product.name + `" class="img-fluid">
         <div class="card-label">
-            <span class="name">`+ content.name + `</span>
-            <span class="price">₱`+ content.price + `</span>
+            <span class="name">`+ product.name + `</span>
+            <span class="price">₱`+ product.price + `</span>
         </div>
     </div>`
     });
---
+++ ADET/A06/products.php
@@ -0,0 +1,45 @@
+<?php
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+include("connect.php");
+
+$method = $_SERVER['REQUEST_METHOD'];
+$input = json_decode(file_get_contents('php://input'), true);
+
+switch ($method) {
+  case 'POST':
+    handlePost($pdo,$input);
+    break;
+    case 'GET':
+    handleGet($pdo, $_GET);
+    break;
+  default:
+    echo json_encode(['message' => 'Invalid request method']);
+    break;
+}
+
+function handlePost($pdo, $input)
+{
+  $sql = "SELECT * FROM products WHERE categoryID = :categoryID";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute(['categoryID' => $input['categoryID']]);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  echo json_encode($result);
+}
+
+function handleGet($pdo, $queryParams)
+{
+    if (isset($queryParams['categoryID'])) {
+        $sql = "SELECT * FROM products WHERE categoryID = :categoryID";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['categoryID' => $_GET['categoryID']]);
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result);
+    } else {
+        echo json_encode(['message' => 'categoryID is required']);
+    }
+}
+
+?>
